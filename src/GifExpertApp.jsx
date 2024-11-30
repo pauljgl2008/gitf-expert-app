e57027bd0667287c490e5940a9557021f153b220
Paúl Guevara <pauljgl2008@gmail.com>
2024-11-30 12:01:05 -0500

    feat: emit an event to parent

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddCategory } from './AddCategory';
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Goku']);
+    const onAddCategory = (newCategory) => {
+        console.log(newCategory)
+        // console.log(document.getElementById('new-category').value);
+        // setCategories([document.getElementById('new-category').value, ...categories])
+        // setCategories(cat => [...cat, document.getElementById('new-category').value])
+        setCategories([newCategory, ...categories])
+    }
     return (
         <>
             <h1>GifExpertApp</h1>
+            {/* <input id="new-category"></input> */}
+            <AddCategory
+                // setCategories={setCategories}
+                onNewCategory={value=> onAddCategory(value)}
+            />
+            {/* <button onClick={onAddCategory}>Agregar</button> */}
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                }
+                )}
+                {/* <li>ABC</li> */}
+            </ol>
         </>
     )
 }
