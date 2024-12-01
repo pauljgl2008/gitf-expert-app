a91d6e24ea79e1c3fb434395c33a5634da887612
Paúl Guevara <pauljgl2008@gmail.com>
2024-11-30 21:16:33 -0500

    feat: show image's titles

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,16 +2,29 @@ import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-    const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState([]);
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
     useEffect(() => {
-        getGifs(category);
+        // getGifs(category)
+        //     .then(newImages => setImages(newImages))
+        getImages()
     }, [])
-    
+
     return (
         <>
             <h3>{category}</h3>
-            <h5>{counter}</h5>
-            <button onClick={()=>setCounter(counter+1)}>+1</button>
+            <ol>
+                {
+                    images.map(({id,title}) =>
+                    (
+                        <li key={id}>{title}</li>
+                    )
+                    )
+                }
+            </ol>
         </>
     )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,13 +2,11 @@ export const getGifs = async (category) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=H85s8nAGcQ0fxrAypCO94Rp9Ly6YBfeT&q=${category}&limit=20`
     const resp = await fetch(url);
     const { data = [] } = await resp.json();
-    console.log(data);
     const gifs = data.map(img => ({
         id: img.id,
         title: img.title,
         url: img.images.downsized_medium.url
     }));
-    console.log("gifs");
     console.log(gifs);
     return gifs;
 }
