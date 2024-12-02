0c8dc3c5c192a57d09baa83c76aff4a9db870ed1
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-01 22:26:51 -0500

    feat: add GifGrid, mock customHook

---
+++ tests/components/GifGrid.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../src/components/GifGrid";
+
+describe('Pruebas en <GifGrid/>', () => { 
+    const category = 'One Punch';
+    test('debe de mostrar el loading inicialmente', () => { 
+        render(<GifGrid category={category}/>);
+        // screen.debug()
+        expect(screen.getByText('Cargando...'))
+        expect(screen.getByText(category))
+     })
+     test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => { second })
+ })
