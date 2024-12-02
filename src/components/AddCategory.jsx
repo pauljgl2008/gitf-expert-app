cabd539a340db7f0f08e835cace83f9fa303fdc9
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-01 20:41:01 -0500

    feat: addCategory test

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { useState } from 'react'
 
 export const AddCategory = ({onNewCategory}) => {
@@ -23,4 +24,8 @@ export const AddCategory = ({onNewCategory}) => {
             />
         </form>
     )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired
 }
