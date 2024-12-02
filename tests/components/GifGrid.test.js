0f2e2da8e26d3cfe89e388269d5258d63c3ab2e7
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-02 00:43:23 -0500

    feat: gifgrid custom hook mock

--- tests/components/GifGrid.test.js
+++ tests/components/GifGrid.test.js
@@ -1,13 +1,41 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
 
-describe('Pruebas en <GifGrid/>', () => { 
+jest.mock('../../src/hooks/useFetchGifs')
+
+describe('Pruebas en <GifGrid/>', () => {
     const category = 'One Punch';
-    test('debe de mostrar el loading inicialmente', () => { 
-        render(<GifGrid category={category}/>);
+    test('debe de mostrar el loading inicialmente', () => {
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
+        render(<GifGrid category={category} />);
         // screen.debug()
         expect(screen.getByText('Cargando...'))
         expect(screen.getByText(category))
-     })
-     test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => { second })
- })
+    })
+    test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => {
+        const gifs = [
+            {
+                id:'support',
+                title:'lion',
+                url:'https://www.dota.com'
+            },
+            {
+                id:'dbs',
+                title:'goku',
+                url:'https://www.dbz.com'
+            }
+        ]
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: true
+        });
+        render(<GifGrid category={category} />);
+        // screen.debug()
+        expect(screen.getAllByRole('img').length).toBe(2);
+    })
+})
